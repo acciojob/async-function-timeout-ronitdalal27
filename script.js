a962468dc25f1307e36b357d1cb4c58b4cae3839
ronitdalal27 <dalalronit131@gmail.com>
2025-01-20 08:04:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',async () => { //using async fun on click of button,bcoz question requires that
+	const text = document.getElementById("text").value;
+	const delay = Number(document.getElementById("delay").value); //Number() to covert input delay into number,as we know input result returns string
+	await delayFunction(delay);
+	document.getElementById("output").innerText = text;
+});
+
+async function delayFunction(delay){
+	return new Promise((resolve) => {
+		setTimeout(resolve,delay)
+	})
+}
